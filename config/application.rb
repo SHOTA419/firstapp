c2362b3ea1dd42ea7b22ec0f9b16ece338fcf457
shota <s.sugimoto.419@gmail.com>
2014-08-20 04:47:21 +0900

    test

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Firstapp
   class Application < Rails::Application
     #for heroku deploy
-    config.assets.initialize_on_precompile = false
+config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
