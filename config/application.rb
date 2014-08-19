7042b2aadddb6989db2d938474168de6ce0a2b2a
shota <s.sugimoto.419@gmail.com>
2014-08-20 04:27:32 +0900

    Update config/application.rb for Heroku

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Firstapp
   class Application < Rails::Application
     #for heroku deploy
     config.assets.initialize_on_precompile = false
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
